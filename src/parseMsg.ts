67f705edad44a719015cb1fc764188a02de62ecc
Phillip Ivan Pietruschka <ivanpietruschka@gmail.com>
2025-06-09 15:27:18 +1000

    udp parsing WIP.

--- src/parseMsg.ts
+++ src/parseMsg.ts
@@ -1,6 +1,14 @@
 import type { AvHsw10 } from './main.js'
 import { MsgSyntax, Messages, ErrorDetails } from './enums.js'
 
+export interface DisplayMessageData {
+	index: number
+	rhTallyLampState: boolean
+	textTallyState: boolean
+	lhTallyLampState: boolean
+	text: string
+}
+
 export const parseTcpMsg = (msg: string, self: AvHsw10): void => {
 	msg = msg.replaceAll(MsgSyntax.Stx, '').replaceAll(MsgSyntax.Etx, '')
 	const data = msg.split(MsgSyntax.Sep)
@@ -57,4 +65,40 @@ export const parseTcpMsg = (msg: string, self: AvHsw10): void => {
 
 export const parseUdpMsg = (msg: Buffer, self: AvHsw10): void => {
 	self.logger.debug(`Parsing UDP message: ${msg}`)
+	const PBC = msg.readUInt16LE(0)
+	if (msg.length !== PBC + 2 || PBC > 2046) {
+		self.logger.warn(`UDP message expected length. Expedted ${PBC + 2}, Recieved: ${msg.length}`)
+		return
+	}
+	const VER = msg.readUint8(2)
+	const FLAGS = msg.readUint8(3)
+	const SCREEN = msg.readUInt16BE(4)
+	if (VER !== 0) {
+		self.logger.warn(`Unexpected VER value in message, expected 0 recieved ${VER}`)
+		return
+	}
+	if ((FLAGS & 2) !== 0) {
+		self.logger.warn(`Message contains Screen Data ${SCREEN}`)
+		return
+	}
+	const dsmg: DisplayMessageData[] = []
+	let position = 6
+	while (position < msg.length) {
+		try {
+			const newDisplayMsg: DisplayMessageData = {
+				index: msg.readUInt16BE(position),
+				rhTallyLampState: Boolean(msg.readUInt16BE(position + 2) & 3),
+				textTallyState: Boolean(msg.readUInt16BE(position + 2) & 12),
+				lhTallyLampState: Boolean(msg.readUInt16BE(position + 2) & 48),
+				text: msg
+					.subarray(position + 6, position + 18)
+					.toString()
+					.trim(),
+			}
+			dsmg.push(newDisplayMsg)
+		} catch {
+			self.logger.debug(`Failed to parse Display Message Data chunk`)
+		}
+		position += 18
+	}
 }
