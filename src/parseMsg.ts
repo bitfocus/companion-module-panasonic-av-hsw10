a80d3046e9742d8b524611d69b74e8fd00ca779f
Phillip Ivan Pietruschka <ivanpietruschka@gmail.com>
2025-07-02 07:20:47 +1000

    revise msg parsing

--- src/parseMsg.ts
+++ src/parseMsg.ts
@@ -1,5 +1,5 @@
 import type { AvHsw10 } from './main.js'
-import { MsgSyntax, Messages, ErrorDetails } from './enums.js'
+import { MsgSyntax } from './enums.js'
 
 export interface DisplayMessageData {
 	index: number
@@ -13,50 +13,46 @@ export const parseTcpMsg = (msg: string, self: AvHsw10): void => {
 	msg = msg.replaceAll(MsgSyntax.Stx, '').replaceAll(MsgSyntax.Etx, '')
 	const data = msg.split(MsgSyntax.Sep)
 	try {
-		if (data[0] in Messages) {
-			switch (data[0] as Messages) {
-				case Messages.AutoTransitionTimeResponse:
-					self.logger.console(`Message type: AutoTransitionTimeResponse`)
-					break
-				case Messages.BusCrosspointResponse:
-					self.logger.console(`Message type: BusCrosspointResponse`)
-					break
-				case Messages.BusSettingResponse:
-					self.logger.console(`Message type: BusSettingResponse`)
-					break
-				case Messages.BusStatusResponse:
-					self.logger.console(`Message type: BusStatusResponse`)
-					break
-				case Messages.BusTransitionStatusResponse:
-					self.logger.console(`Message type: BusTransitionStatusResponse`)
-					break
-				case Messages.KeySignalCouplingResponse:
-					self.logger.console(`Message type: KeySignalCouplingResponse`)
-					break
-				case Messages.PinPStatusResponse:
-					self.logger.console(`Message type: PinPStatusResponse`)
-					break
-				case Messages.SourceNameResponse:
-					self.logger.console(`Message type: SourceNameResponse`)
-					break
-				case Messages.TransitionPatternResponse:
-					self.logger.console(`Message type: TransitionPatternResponse`)
-					break
-				case Messages.Error:
-					self.logger.console(`Message type: Error`)
-					switch (data[1]) {
-						case ErrorDetails.OutOfRangeParameter as string:
-							self.logger.warn('Error, Parameter out of range')
-							break
-						case ErrorDetails.SyntaxError as string:
-							self.logger.warn('Syntax Error')
-					}
-					break
-				default:
-					self.logger.debug(`Message unexpectedly Control or Query type: ${data[0]}`)
-			}
-		} else {
-			self.logger.warn(`Unrecognised message type: ${data[0]} from: ${msg}`)
+		switch (data[0]) {
+			case 'ATIM':
+				self.logger.console(`Message type: AutoTransitionTimeResponse`)
+				break
+			case 'ABST':
+				self.logger.console(`Message type: BusCrosspointResponse`)
+				break
+			case 'ABUS':
+				self.logger.console(`Message type: BusSettingResponse`)
+				break
+			case 'ABSC':
+				self.logger.console(`Message type: BusStatusResponse`)
+				break
+			case 'ABTI':
+				self.logger.console(`Message type: BusTransitionStatusResponse`)
+				break
+			case 'AKRS':
+				self.logger.console(`Message type: KeySignalCouplingResponse`)
+				break
+			case 'APNP':
+				self.logger.console(`Message type: PinPStatusResponse`)
+				break
+			case 'ASNM':
+				self.logger.console(`Message type: SourceNameResponse`)
+				break
+			case 'APAT':
+				self.logger.console(`Message type: TransitionPatternResponse`)
+				break
+			case 'EROR':
+				self.logger.console(`Message type: Error`)
+				switch (data[1]) {
+					case '01':
+						self.logger.warn('Error, Parameter out of range')
+						break
+					case '02':
+						self.logger.warn('Syntax Error')
+				}
+				break
+			default:
+				self.logger.debug(`Message unexpectedly Control or Query type: ${data[0]}`)
 		}
 	} catch (e) {
 		self.logger.warn(`Message parsing failed: ${e}`)
