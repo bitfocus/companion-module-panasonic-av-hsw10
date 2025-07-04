49ca5afcf165b6893010b44698cce779d67ff010
Phillip Ivan Pietruschka <ivanpietruschka@gmail.com>
2025-07-04 18:07:54 +1000

    rework udp msg parsing

--- src/parseMsg.ts
+++ src/parseMsg.ts
@@ -60,41 +60,6 @@ export const parseTcpMsg = (msg: string, self: AvHsw10): void => {
 }
 
 export const parseUdpMsg = (msg: Buffer, self: AvHsw10): void => {
-	self.logger.debug(`Parsing UDP message: ${msg}`)
-	const PBC = msg.readUInt16LE(0)
-	if (msg.length !== PBC + 2 || PBC > 2046) {
-		self.logger.warn(`UDP message expected length. Expected ${PBC + 2}, Recieved: ${msg.length}`)
-		return
-	}
-	const VER = msg.readUint8(2)
-	const FLAGS = msg.readUint8(3)
-	const SCREEN = msg.readUInt16BE(4)
-	if (VER !== 0) {
-		self.logger.warn(`Unexpected VER value in message, expected 0 recieved ${VER}`)
-		return
-	}
-	if ((FLAGS & 2) !== 0) {
-		self.logger.warn(`Message contains Screen Data ${SCREEN}`)
-		return
-	}
-	const dsmg: DisplayMessageData[] = []
-	let position = 6
-	while (position < msg.length) {
-		try {
-			const newDisplayMsg: DisplayMessageData = {
-				index: msg.readUInt16BE(position),
-				rhTallyLampState: Boolean(msg.readUInt16BE(position + 2) & 3),
-				textTallyState: Boolean(msg.readUInt16BE(position + 2) & 12),
-				lhTallyLampState: Boolean(msg.readUInt16BE(position + 2) & 48),
-				text: msg
-					.subarray(position + 6, position + 18)
-					.toString()
-					.trim(),
-			}
-			dsmg.push(newDisplayMsg)
-		} catch {
-			self.logger.debug(`Failed to parse Display Message Data chunk`)
-		}
-		position += 18
-	}
+	self.logger.debug(`Parsing UDP message: ${String.fromCodePoint(...msg)}`)
+	parseTcpMsg(String.fromCodePoint(...msg), self)
 }
